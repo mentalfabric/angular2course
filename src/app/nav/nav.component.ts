86899e2736230e1871b8f29c0a782b16467bf657
mentalfabric <koomjianschool@gmail.com>
2017-05-23 20:57:11 -0700

    Day 3

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,8 +1,26 @@
 import {Component} from "@angular/core";
 
+interface Navigation {
+    text: string,
+    url: string
+}
+
 @Component({
     selector : "gw-nav",
     templateUrl : "./nav.html"
 })
 
-export class NavComponent {}
+export class NavComponent {
+    navItems : Navigation[] = [{
+            text : "blah",
+            url: "blah@blah.com"
+    },
+    {
+        text : "two",
+        url : "two@two.com"
+    },
+    {
+        text: "three",
+        url: "three@three.com"
+    }]
+}
